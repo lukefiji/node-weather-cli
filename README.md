3455a99dd148870afba6ced04e73c4edb958a531
Luke Fiji <luke.fiji@gmail.com>
2017-06-25 23:45:37 -0700

    Fetches address dynamically via -a or --address flag

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Node Weather App
-A weather CLI app made in node that uses Google's Geolocation API to find the user's location based on address.
+A weather CLI app made in Node that uses Google's Geolocation API to find the user's location based on address.
--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const request = require("request");
+const yargs = require("yargs");
 
-// Make a request
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather for",
+      string: true // Always parse as a string
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
+
+console.log(argv);
+
+const encodedAddress = encodeURIComponent(argv.address);
+
+// Make a request from Google Geolocation
 request(
   {
-    url:
-      "https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20st%20philadelphia",
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
   },
   (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
   }
 );
